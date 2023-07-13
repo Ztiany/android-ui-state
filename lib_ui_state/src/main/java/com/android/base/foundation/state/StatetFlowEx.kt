43df31600c45b8310b67bf993021dad79dd66fd1
Alien <ztiany3@gmail.com>
2023-07-13 17:23:30 +0800

    optimize extension methods

--- lib_ui_state/src/main/java/com/android/base/foundation/state/StatetFlowEx.kt
+++ lib_ui_state/src/main/java/com/android/base/foundation/state/StatetFlowEx.kt
@@ -1,29 +1,6 @@
 package com.android.base.foundation.state
 
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.update
-
-fun <L : Any?, D : Any?, E : Any?> MutableStateFlow<State<L, D, E>>.updateToLoading(step: L? = null) {
-    update { State.loading(step) }
-}
-
-fun <L : Any?, D : Any?, E : Any?> MutableStateFlow<State<L, D, E>>.updateToError(error: Throwable) {
-    update { State.error(error) }
-}
-
-fun <L : Any?, D : Any?, E : Any?> MutableStateFlow<State<L, D, E>>.updateToData(data: D?) {
-    val state: State<L, D, E> = if (data == null) {
-        State.noData()
-    } else {
-        State.success(data)
-    }
-    update { state }
-}
-
-fun <L : Any?, D : Any?, E : Any?> MutableStateFlow<State<L, D, E>>.updateToSuccess() {
-    val state: State<L, D, E> = State.noData()
-    update { state }
-}
 
 fun <L : Any?, D : Any?, E : Any?> MutableStateFlow<State<L, D, E>>.setLoading(step: L? = null) {
     value = State.loading(step)
